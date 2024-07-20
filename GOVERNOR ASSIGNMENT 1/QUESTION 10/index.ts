3eff0482a25e6e1d25a369309485847d861cfbe5
SyedAnas-123 <syedmohammadanasahmed@gmail.com>
2024-07-20 16:47:59 +0500

    Update index.ts

---
+++ GOVERNOR ASSIGNMENT 1/QUESTION 10/index.ts
@@ -0,0 +1,6 @@
+//AUTHOR : SYED ANAS
+//DATE :14/3/2024
+//DESCRIPTION : THIS IS MY FAV NUM WHIHC I STIRED INN FAV NUM VARIABLE 
+
+let favNum : number = 3
+console.log(`MY FAV NUMBER IS ${favNum}`)
